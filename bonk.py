18edd88e317a831af716c3b395deae0bc6936e25
DiogoS5 <101642268+DiogoS5@users.noreply.github.com>
2022-03-17 18:03:17 +0000

    Update bonk.py

--- bonk.py
+++ bonk.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import random, os
+from math import sqrt
 
 pg.init()
 
@@ -28,17 +29,16 @@ SCREEN.fill((BLACK))
 ball_width = 10
 ball_height = 10
 ball = pg.Rect((WIDTH-ball_width)/2, (HEIGHT-ball_height)/2, ball_width, ball_height)
-ball_speed_x = 10
+ball_speed = 7
 
 #Paddles
 pad_width = 10 
 pad_height = 80
+pad_speed = 5
 #Paddle 1
 pad1 = pg.Rect(50,(HEIGHT-pad_height)/2, pad_width, pad_height)
-pad1_speed_y = 0
 #Paddle 2
 pad2 = pg.Rect(WIDTH-50,(HEIGHT-pad_height)/2, pad_width, pad_height)
-pad2_speed_y = 0
 
 '''---------------------------------------------------------------'''
 
@@ -53,8 +53,9 @@ while run:
     if reset:
         ball.x = (WIDTH-ball_width)/2  
         ball.y = (HEIGHT-ball_height)/2
+        ball_speed_y = random.randint(int((-ball_speed/2)), int(ball_speed/2))
+        ball_speed_x = int(sqrt(ball_speed**2 - abs(ball_speed_y)**2))
         ball_speed_x *= -1
-        ball_speed_y = random.randint(-5, 5)
         reset = False
     #Events
     for event in pg.event.get():
@@ -73,20 +74,26 @@ while run:
     ball.y += ball_speed_y
     #Pads movement
     if keys[pg.K_w] and pad1.top > 0:
-        pad1.y -= 10
+        pad1.y -= pad_speed
     if keys[pg.K_s] and pad1.bottom < HEIGHT:
-        pad1.y += 10
+        pad1.y += pad_speed
     if keys[pg.K_UP] and pad2.top > 0:
-        pad2.y -= 10
+        pad2.y -= pad_speed
     if keys[pg.K_DOWN] and pad2.bottom < HEIGHT:
-        pad2.y += 10
+        pad2.y += pad_speed
 
     #Collisions
-    if pg.Rect.colliderect(ball, pad1) or pg.Rect.colliderect(ball, pad2):
-        ball_speed_x *= -1
+    if pg.Rect.colliderect(ball, pad1) and pad1.bottom > ball.centery and pad1.top < ball.centery:
+        ratio = (ball.centery - pad1.centery)/(pad_height/2)
+        ball_speed_y = int(ratio*abs(ball_speed/sqrt(2)))
+        ball_speed_x = int(sqrt(ball_speed**2 - ball_speed_y**2))
+    if pg.Rect.colliderect(ball, pad2) and pad2.bottom > ball.centery and pad2.top < ball.centery:
+        ratio = (ball.centery - pad2.centery)/(pad_height/2)
+        ball_speed_y = ratio*abs(ball_speed_x)
+        ball_speed_x = int(sqrt(ball_speed**2 - ball_speed_y**2))*-1
+    
     if ball.top <= 0 or ball.bottom >= HEIGHT:
         ball_speed_y *= -1
-
     #End
     if ball.left <= 0 or ball.right >= WIDTH:
         reset = True
