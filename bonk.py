9ea939b798d8714e3ab5836112dd45342f7ae603
DiogoS5 <101642268+DiogoS5@users.noreply.github.com>
2022-03-22 20:52:11 +0000

    Update bonk.py

--- bonk.py
+++ bonk.py
@@ -24,31 +24,35 @@ SCREEN = pg.display.set_mode([WIDTH, HEIGHT])
 pg.display.set_caption("Bonk")
 SCREEN.fill((BLACK))
 
-#MENU
+#MENU SETUP
 #Buttons
 font = pg.font.SysFont('suigeneris', 60)
-start = font.render('START', True, GREEN)
+start = font.render('START', True, WHITE)
 startRect = start.get_rect()
 startRect.center = (WIDTH/2, HEIGHT/2)
 
+#GAME SETUP
 #countdown
-three = font.render('3', True, color)
-threeRect = three.get_rect()
-threeRect.center = (WIDTH/2, HEIGHT/3)
-two = font.render('2', True, color)
-twoRect = two.get_rect()
-twoRect.center = (WIDTH/2, HEIGHT/3)
-one = font.render('1', True, color)
-oneRect = one.get_rect()
-oneRect.center = (WIDTH/2, HEIGHT/3)
+count = '3'
+centertxt = font.render(count, True, WHITE)
+centertxtRect = centertxt.get_rect()
+centertxtRect.center = (WIDTH/2, HEIGHT/3)
+#Scoreboard
+score1 = 0
+score1txt = font.render(str(score1), True, WHITE)
+score1txtRect = score1txt.get_rect()
+score1txtRect.center = (200, 50)
+score2 = 0
+score2txt = font.render(str(score2), True, WHITE)
+score2txtRect = score2txt.get_rect()
+score2txtRect.center = (WIDTH-200, 50)
 
-#OBJECTS:
 #Ball
 ball_width = 10
 ball_height = 10
 ball = pg.Rect((WIDTH-ball_width)/2, (HEIGHT-ball_height)/2, ball_width, ball_height)
-ball_speed = 8
-
+ball_speed_default = 7
+ball_speed = ball_speed_default
 #Paddles
 pad_width = 10 
 pad_height = 80
@@ -58,94 +62,145 @@ pad1 = pg.Rect(50,(HEIGHT-pad_height)/2, pad_width, pad_height)
 #Paddle 2
 pad2 = pg.Rect(WIDTH-50,(HEIGHT-pad_height)/2, pad_width, pad_height)
 
+#Settings
 click = (0, 0)
 clock = pg.time.Clock()
 SCREEN.fill((BLACK))
 
-#Menu
-menu = True
+
+#Run Loop
 run = True
-while menu:
-    clock.tick(FPS)
-    #Events
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            menu = False
-        if event.type == pg.MOUSEBUTTONDOWN:
-            click = pg.mouse.get_pos()
-    #Buttons
-    SCREEN.blit(start, startRect)
-    #Clicks
-    if startRect.left <= click[0] <= startRect.right and startRect.top <= click[1] <= startRect.bottom:
-        menu = False
-    pg.display.update()
-
-
-#Game Loop
-reset = True
-reset_time = -1500
+menu = True
+game = False
 while run:
-    clock.tick(FPS)
-    SCREEN.fill((BLACK))    
-    #Draw
-    pg.draw.rect(SCREEN, color, pad1)
-    pg.draw.rect(SCREEN, color, pad2)
-    pg.draw.rect(SCREEN, color, ball)
+    #Menu
+    while menu:
+        clock.tick(FPS)
+        SCREEN.fill(BLACK)
+        #Events
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                menu = False
+                run = False
+            if event.type == pg.MOUSEBUTTONDOWN:
+                click = pg.mouse.get_pos()
+        #Buttons
+        SCREEN.blit(start, startRect)
+        #Clicks
+        if startRect.left <= click[0] <= startRect.right and startRect.top <= click[1] <= startRect.bottom:
+            click = (0,0)
+            menu = False
+            game = True
+        pg.display.update()
+        
+    #Game Loop
+    reset = True
+    reset_time = -1500
+    hits = 0
+    while game:
+        clock.tick(FPS)
+        SCREEN.fill((BLACK))
+        score1txt = font.render(str(score1), True, WHITE)
+        score2txt = font.render(str(score2), True, WHITE)
+        SCREEN.blit(score1txt, score1txtRect)
+        SCREEN.blit(score2txt, score2txtRect)    
+        #Draw
+        pg.draw.rect(SCREEN, color, pad1)
+        pg.draw.rect(SCREEN, color, pad2)
+        pg.draw.rect(SCREEN, WHITE, ball)
 
-    #Reset
-    if reset:
-        ball.x = (WIDTH-ball_width)/2  
-        ball.y = (HEIGHT-ball_height)/2
-        ball_speed_x = 0
-        ball_speed_y = 0
-        time_delta = pg.time.get_ticks() - reset_time
-        if time_delta < 500:
-            SCREEN.blit(three, threeRect)
-        elif time_delta < 1000:
-            SCREEN.blit(two, twoRect)
-        elif time_delta < 1500:
-            SCREEN.blit(one, oneRect)
-        elif time_delta >= 1500:
-            SCREEN.blit(one, oneRect)
-            ball_speed_y = random.randint(int((-ball_speed/2)), int(ball_speed/2))
-            ball_speed_x = int(sqrt(ball_speed**2 - abs(ball_speed_y)**2))
-            ball_speed_x *= -1
-            reset = False
-    #Events
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            run = False
-    keys = pg.key.get_pressed()
+        #Reset
+        if reset:
+            if score1 == 1:
+                centertxt = font.render('PLAYER 1 WINS!', True, WHITE)
+                centertxtRect = centertxt.get_rect()
+                centertxtRect.center = (WIDTH/2, HEIGHT/3)
+                SCREEN.blit(centertxt, centertxtRect)
+                time_delta = pg.time.get_ticks() - reset_time
+                if time_delta > 3000:
+                    score1 = 0
+                    score2 = 0
+                    menu = True
+                    reset = False
+                    game = False
+            elif score2 == 1:
+                centertxt = font.render('PLAYER 2 WINS!', True, WHITE)
+                centertxtRect = centertxt.get_rect()
+                centertxtRect.center = (WIDTH/2, HEIGHT/3)
+                SCREEN.blit(centertxt, centertxtRect)
+                time_delta = pg.time.get_ticks() - reset_time
+                if time_delta > 3000:
+                    score1 = 0
+                    score2 = 0
+                    menu = True
+                    reset = False
+                    game = False
+            else:
+                time_delta = pg.time.get_ticks() - reset_time
+                centertxt = font.render(count, True, WHITE)
+                SCREEN.blit(centertxt, centertxtRect)
+                if time_delta < 500:
+                    count = '3'
+                elif time_delta < 1000:
+                    count = '2'
+                elif time_delta < 1500:
+                    count = '1'
+                elif time_delta >= 1500:
+                    ball_speed_y = random.randint(int((-ball_speed/4)), int(ball_speed/4))
+                    ball_speed_x = int(sqrt(ball_speed**2 - abs(ball_speed_y)**2))
+                    ball_speed_x *= random.choice([-1, 1])
+                    reset = False
+        #Events
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                game = False
+                run = False
+        keys = pg.key.get_pressed()
 
-    #Ball movement
-    ball.x += ball_speed_x
-    ball.y += ball_speed_y
-    #Pads movement
-    if keys[pg.K_w] and pad1.top > 0:
-        pad1.y -= pad_speed
-    if keys[pg.K_s] and pad1.bottom < HEIGHT:
-        pad1.y += pad_speed
-    if keys[pg.K_UP] and pad2.top > 0:
-        pad2.y -= pad_speed
-    if keys[pg.K_DOWN] and pad2.bottom < HEIGHT:
-        pad2.y += pad_speed
+        #Ball movement
+        ball.x += ball_speed_x
+        ball.y += ball_speed_y
+        #Pads movement
+        if keys[pg.K_w] and pad1.top > 0:
+            pad1.y -= pad_speed
+        if keys[pg.K_s] and pad1.bottom < HEIGHT:
+            pad1.y += pad_speed
+        if keys[pg.K_UP] and pad2.top > 0:
+            pad2.y -= pad_speed
+        if keys[pg.K_DOWN] and pad2.bottom < HEIGHT:
+            pad2.y += pad_speed
 
-    #Collisions
-    if pg.Rect.colliderect(ball, pad1) and pad1.bottom > ball.centery and pad1.top < ball.centery:
-        ratio = (ball.centery - pad1.centery)/(pad_height/2)
-        ball_speed_y = int(ratio*abs(ball_speed/sqrt(2)))
-        ball_speed_x = int(sqrt(ball_speed**2 - ball_speed_y**2))
-    if pg.Rect.colliderect(ball, pad2) and pad2.bottom > ball.centery and pad2.top < ball.centery:
-        ratio = (ball.centery - pad2.centery)/(pad_height/2)
-        ball_speed_y = ratio*abs(ball_speed_x)
-        ball_speed_x = int(sqrt(ball_speed**2 - ball_speed_y**2))*-1
-    
-    if ball.top <= 0 or ball.bottom >= HEIGHT:
-        ball_speed_y *= -1
-    #End
-    if ball.left <= 0 or ball.right >= WIDTH:
-        reset = True
-        reset_time = pg.time.get_ticks()
-    pg.display.update()
+        #Collisions
+        if pg.Rect.colliderect(ball, pad1) and pad1.bottom > ball.centery and pad1.top < ball.centery:
+            hits += 1
+            if hits%3 == 0:
+                ball_speed += 1
+            ratio = (ball.centery - pad1.centery)/(pad_height/2)
+            ball_speed_y = int(ratio*abs(ball_speed/sqrt(2)))
+            ball_speed_x = int(sqrt(ball_speed**2 - ball_speed_y**2))
+        if pg.Rect.colliderect(ball, pad2) and pad2.bottom > ball.centery and pad2.top < ball.centery:
+            hits += 1
+            if hits%5 == 0:
+                ball_speed += 1
+            ratio = (ball.centery - pad2.centery)/(pad_height/2)
+            ball_speed_y = ratio*abs(ball_speed_x)
+            ball_speed_x = int(sqrt(ball_speed**2 - ball_speed_y**2))*-1
+        if ball.top <= 0 or ball.bottom >= HEIGHT:
+            ball_speed_y *= -1
 
+        #End/Score
+        if ball.left <= 0 or ball.right >= WIDTH:
+            if ball.left <= 0:
+                score2 += 1
+            else:
+                score1 += 1
+            reset = True
+            ball.x = (WIDTH-ball_width)/2  
+            ball.y = (HEIGHT-ball_height)/2
+            ball_speed = ball_speed_default
+            ball_speed_x = 0
+            ball_speed_y = 0
+            reset_time = pg.time.get_ticks()
+
+        pg.display.update()
 pg.quit()
